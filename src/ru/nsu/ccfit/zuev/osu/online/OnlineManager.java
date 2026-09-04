d72158b604cc2fef500c7e61557a678a2404daf0
gammadan2 <federicoataana2@gmail.com>
2026-09-04 19:33:31 +0800

    7273

--- src/ru/nsu/ccfit/zuev/osu/online/OnlineManager.java
+++ src/ru/nsu/ccfit/zuev/osu/online/OnlineManager.java
@@ -33,7 +33,7 @@ public class OnlineManager {
    public static final String hostname = "omission-badge-hazy.ngrok-free.dev";
    public static final String endpoint = "https://" + hostname + "/api/";
    public static final String updateEndpoint = endpoint + "update.php?lang=";
-
+   public static final String defaultAvatarURL = "https://" + hostname + "/user/avatar/0.png";
     private static final String onlineVersion = "9";
 
     public static final OkHttpClient client = new OkHttpClient();
